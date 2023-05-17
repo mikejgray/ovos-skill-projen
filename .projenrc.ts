2d252388e541ac8e18c20b959a6ed1cd75cb106a
Mike Gray <mike@graywind.org>
2023-05-16 21:48:03 -0500

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,15 @@
+import { cdk } from 'projen';
+const project = new cdk.JsiiProject({
+  author: 'Mike Gray',
+  authorAddress: 'mike@graywind.org',
+  defaultReleaseBranch: 'main',
+  name: 'ovos-skill',
+  projenrcTs: true,
+  repositoryUrl: 'https://github.com/mike/ovos-skill.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
